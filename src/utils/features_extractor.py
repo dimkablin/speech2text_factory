b6fa5a22803a49c3b92456800743ca9ad65ef065
Dimka <dmitryellison@gmail.com>
2024-03-22 16:19:32 +0300

    bugsixing/added stere2mono convertion

--- src/utils/features_extractor.py
+++ src/utils/features_extractor.py
@@ -1,14 +1,24 @@
 """Feture extractor utilities"""
 from os import PathLike
 from typing import BinaryIO
+import numpy as np
 import torch
 import torchaudio
+from pydub import AudioSegment
 
 
-def load_audio(file: BinaryIO | str | PathLike) -> torch.Tensor:
+def stereo2mono(audio: torch.Tensor) -> torch.Tensor:
+    """Convert audio from stereo to mono."""
+    audio_mono = torch.mean(audio, dim=0)
+
+    return audio_mono
+
+
+def load_audio(path: str | PathLike) -> torch.Tensor:
     """ Load audio from file."""
     # load our wav file
-    speech, sr = torchaudio.load(file)
+    speech, sr = torchaudio.load(path)
+    speech = stereo2mono(speech)
     resampler = torchaudio.transforms.Resample(sr, 16000)
     speech = resampler(speech)
     return speech.squeeze()
